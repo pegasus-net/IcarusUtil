61e7428b1ffd991a92e31172b96bdc1f89f966da
廖强 <1767796806@qq.com>
2021-07-14 18:14:02 +0800

    -

--- pegasus/src/main/java/a/icarus/utils/Logger.java
+++ pegasus/src/main/java/a/icarus/utils/Logger.java
@@ -1,6 +1,6 @@
 package a.icarus.utils;
 
-import android.content.Context;
+import android.text.TextUtils;
 import android.util.Log;
 
 import java.io.File;
@@ -8,6 +8,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 @SuppressWarnings("unused")
 public class Logger {
@@ -19,8 +22,9 @@ public class Logger {
     public static final int ERROR = 1 << 5;
     public static final int CRASH = 1 << 6;
 
-    private static String TAG = "Logger:TAG-->";
-    private static int level = INFO | WARN | ERROR ;
+    private static String TAG = "Logger:TAG -->";
+    private static final String EMPTY = "empty";
+    private static int level = INFO | WARN | ERROR;
     private static int type = INFO;
 
     public static void t(Object... arr) {
@@ -45,27 +49,47 @@ public class Logger {
 
     public static void v(Object... arr) {
         if ((level & VERBOSE) == 0) return;
-        Log.v(TAG, Strings.concat(arr));
+        String msg = Strings.concat(arr);
+        if (TextUtils.isEmpty(msg)) {
+            msg = EMPTY;
+        }
+        Log.v(TAG, msg);
     }
 
     public static void d(Object... arr) {
         if ((level & DEBUG) == 0) return;
-        Log.d(TAG, Strings.concat(arr));
+        String msg = Strings.concat(arr);
+        if (TextUtils.isEmpty(msg)) {
+            msg = EMPTY;
+        }
+        Log.d(TAG, msg);
     }
 
     public static void i(Object... arr) {
         if ((level & INFO) == 0) return;
-        Log.i(TAG, Strings.concat(arr));
+        String msg = Strings.concat(arr);
+        if (TextUtils.isEmpty(msg)) {
+            msg = EMPTY;
+        }
+        Log.i(TAG, msg);
     }
 
     public static void w(Object... arr) {
         if ((level & WARN) == 0) return;
-        Log.w(TAG, Strings.concat(arr));
+        String msg = Strings.concat(arr);
+        if (TextUtils.isEmpty(msg)) {
+            msg = EMPTY;
+        }
+        Log.w(TAG, msg);
     }
 
     public static void e(Object... arr) {
         if ((level & ERROR) == 0) return;
-        Log.e(TAG, Strings.concat(arr));
+        String msg = Strings.concat(arr);
+        if (TextUtils.isEmpty(msg)) {
+            msg = EMPTY;
+        }
+        Log.e(TAG, msg);
     }
 
     public static void save(Throwable e) {
@@ -75,14 +99,16 @@ public class Logger {
         if (!logDir.exists()) {
             boolean b = logDir.mkdir();
         }
-        File log = new File(logDir, Strings.concat(System.currentTimeMillis() / 1000,
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd%HH_mm_ss", Locale.CHINA);
+        String dateString = format.format(date);
+        File log = new File(logDir, Strings.concat(dateString,
                 e.getClass().getSimpleName(), ".log"));
         FileOutputStream fos = null;
         try {
             fos = new FileOutputStream(log);
             save(e, fos);
-        } catch (FileNotFoundException fileNotFoundException) {
-            fileNotFoundException.printStackTrace();
+        } catch (Exception ignore) {
         } finally {
             Recycle.close(fos);
         }
