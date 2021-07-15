10a0b4e6bbffb542346ae94773e5542b92b89f26
廖强 <1767796806@qq.com>
2021-07-15 14:43:41 +0800

    -

--- pegasus/src/main/java/a/icarus/utils/FileProviderSupport.java
+++ pegasus/src/main/java/a/icarus/utils/FileProviderSupport.java
@@ -1,8 +1,16 @@
 package a.icarus.utils;
 
+import android.util.Log;
+
 import androidx.core.content.FileProvider;
 
 public class FileProviderSupport extends FileProvider {
+    @Override
+    public boolean onCreate() {
+        Log.i("TAG", "onCreate: ");
+        return super.onCreate();
+    }
+
     public static String AUTHORITIES() {
         return Icarus.getContext().getPackageName() + ".icarus._file_provider";
     }
