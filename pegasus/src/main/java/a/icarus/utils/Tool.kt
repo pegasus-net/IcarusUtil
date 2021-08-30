bb8a2e5cbda9a44d3f11ef5f2e87de183e9ec110
廖强 <1767796806@qq.com>
2021-08-30 16:08:17 +0800

    -

---
+++ pegasus/src/main/java/a/icarus/utils/Tool.kt
@@ -0,0 +1,119 @@
+@file:JvmName("Tool")
+
+package a.icarus.utils
+
+import a.icarus.utils.*
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import org.greenrobot.eventbus.EventBus
+import java.util.*
+import kotlin.random.Random
+
+fun getAppContext(): Context = Icarus.getContext()
+fun getAppResources(): Resources = getAppContext().resources
+
+fun Int.toPx() =
+        ConversionTool.dp2px(this.toFloat())
+
+fun Int.toPxF() =
+        ConversionTool.dp2pxF(this.toFloat())
+
+fun Float.toPx() =
+        ConversionTool.dp2px(this)
+
+fun Float.toPxF() =
+        ConversionTool.dp2pxF(this)
+
+fun Double.percent(): Boolean {
+    return Random.nextDouble() <= this
+}
+
+fun mainThread(run: () -> Unit) {
+    ThreadManager.runOnUiThread(run)
+}
+
+fun subThread(run: () -> Unit) {
+    ThreadManager.runOnThreadPool(run)
+}
+
+fun <T> T.log() {
+    Logger.t(this)
+}
+
+fun <T> T.print() {
+    println(this)
+}
+
+fun <T> T.show() {
+    ToastUtil.show(this.toString())
+}
+
+fun time() {
+    Logger.t(System.currentTimeMillis())
+}
+
+
+fun Activity.requestAllPermission(): Int {
+    try {
+        val packageInfo: PackageInfo =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS)
+        val requestedPermissions = packageInfo.requestedPermissions
+
+        val mPermissionList = ArrayList<String>()
+        requestedPermissions.forEach {
+            if (ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED) {
+                mPermissionList.add(it)
+            }
+        }
+        val permissionArray = mPermissionList.toTypedArray()
+
+        if (mPermissionList.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionArray, 1000)
+            return 1000
+        }
+        return 0
+
+    } catch (e: PackageManager.NameNotFoundException) {
+        e.printStackTrace()
+        return -1
+    }
+}
+
+fun Fragment.startActivity(clazz: Class<out Activity>) {
+    val intent = Intent(this.context, clazz)
+    startActivity(intent)
+}
+
+fun Activity.startActivity(clazz: Class<out Activity>) {
+    val intent = Intent(this, clazz)
+    startActivity(intent)
+}
+
+fun View.visible(b: Boolean) {
+    visibility = if (b) View.VISIBLE else View.GONE
+}
+
+fun <T> T.registerEvent() {
+    EventBus.getDefault().register(this)
+}
+
+fun <T> T.unregisterEvent() {
+    EventBus.getDefault().unregister(this)
+}
+
+fun Context.checkStoragePermission(): Boolean {
+    val read = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    val write = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    return read == PackageManager.PERMISSION_GRANTED && write == PackageManager.PERMISSION_GRANTED
+}
+
+
